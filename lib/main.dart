96fb724d069e1515d5aad30a80f031aeb7d0cba1
borshevskiy <sk8er1001@mail.ru>
2022-12-29 13:23:00 +0600

    Ready

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,128 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<StatefulWidget> createState() {
+    return _MyAppState();
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class _MyAppState extends State<MyApp> {
+  // ignore: prefer_typing_uninitialized_variables
+  var _backgroundColor;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void initState() {
+    _backgroundColor = Colors.red;
+    super.initState();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _changeColor(int page) {
     setState(() {
-      _counter++;
+      List colors = [
+        Colors.red,
+        Colors.redAccent,
+        Colors.deepPurpleAccent,
+        Colors.red,
+        Colors.teal,
+        Colors.blue,
+        Colors.deepPurple];
+
+      _backgroundColor = colors[page];
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white12,
+        body: Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+                alignment: Alignment.bottomLeft,
+                child: ClipPath(
+                    clipper: TriangleClipper(),
+                    child: Container(width: 500, height: 400, color: _backgroundColor)
+                )),
+            Column(
+              children: [
+                Padding(
+                    padding: const EdgeInsets.only(top: 60),
+                    child: Image.asset('assets/marvel.png', width: 200, height: 50)
+                ),
+                const Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                        'Choose your hero',
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold))),
+                SizedBox(
+                  width: 400,
+                  height: 400,
+                  child: PageView(
+                    controller: PageController(viewportFraction: 0.7),
+                    onPageChanged: _changeColor,
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      buildCard('assets/deadpool.jpg', 'Deadpool'),
+                      buildCard('assets/ironman.jpg', 'Ironman'),
+                      buildCard('assets/captainamerica.jpeg', 'Captain America'),
+                      buildCard('assets/spiderman.jpg', 'Spiderman'),
+                      buildCard('assets/doctorstrange.jpg', 'Doctor Strange'),
+                      buildCard('assets/thor.jpg', 'Thor'),
+                      buildCard('assets/thanos.webp', 'Thanos')
+                    ],
+                  ),
+                ),
+              ],)
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
 }
+
+Widget buildCard(String imageUrl, String heroName) => Card(
+  clipBehavior: Clip.antiAlias,
+  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+  child: Stack(
+    alignment: Alignment.center,
+    children: [
+      Image.asset(imageUrl, width: 300, height: 400, fit: BoxFit.cover),
+      Container(
+          padding: const EdgeInsets.all(20),
+          alignment: Alignment.bottomLeft,
+          child: Text(
+              heroName,
+              style: const TextStyle(
+                  fontSize: 20, color: Colors.white,
+                  fontWeight: FontWeight.bold)))
+    ],
+  ),
+);
+
+class TriangleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => false;
+}
+
+
